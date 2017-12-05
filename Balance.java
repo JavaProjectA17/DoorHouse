e06a95353e0974b72dded434ce0c3dae56001c05
iryna <irynka.vlasova@gmail.com>
2017-12-05 14:38:01 +0200

    customer-shop-balance

---
+++ Balance.java
@@ -0,0 +1,12 @@
+import java.util.Date;
+
+public class Balance {
+    public Balance(String name, int sum, Date date){
+       this.name = name;
+       this.sum = sum;
+       this.date = date;
+    }
+    private String name;
+    private int sum;
+    private Date date;
+}
---
+++ Customer.java
@@ -0,0 +1,10 @@
+public class Customer {
+    public Customer(String name,String address, int numberOfPhone){
+        this.name = name;
+        this.address = address;
+        this.numberOfPhone = numberOfPhone;
+    }
+    private String name;
+    private String address;
+    private int numberOfPhone;
+}
